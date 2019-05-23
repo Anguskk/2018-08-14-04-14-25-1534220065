9d98e7081537ceb3ecda6e8ee82f8269dc0a5bab
AngusKK <604202346@qq.com>
2019-05-24 00:03:08 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
+module.exports = function main(str) {
     console.log("Debug Info");
-    return 'Hello World!';
+    var ans="";
+    var arr=[];
+    var top=    ["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
+    var middle= ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+    var bottom= ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+    //._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+    //|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+    //|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+    for (var i = 0; i < str.length; i++) {
+      arr.push(str[i]);
+    }
+    //拼接 ans字符串
+    ans +=  top[arr[0]];
+    for (var i = 1; i < arr.length; i++) {
+      ans += " ";
+      ans += top[arr[i]];
+    }
+    ans += "\n";
+    ans +=  middle[arr[0]];
+    for (var i = 1; i < arr.length; i++) {
+      ans += " ";
+      ans += middle[arr[i]];
+    }
+    ans += "\n";
+    ans +=  bottom[arr[0]];
+    for (var i = 1; i < arr.length; i++) {
+      ans += " ";
+      ans += bottom[arr[i]];
+    }
+    ans += "\n";
+
+    return ans;
 };
